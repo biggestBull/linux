0ebba748678ddb3395dcb293be58a0f6b193afe5
myster liu <2249470506@qq.com>
2022-10-23 21:57:10 +0800

    spider first step!

--- c++/stock_picker/include/spider.hpp
+++ c++/stock_picker/include/spider.hpp
@@ -18,7 +18,7 @@ private:
 
 		r = size * nmemb;
 		strncpy ( out, in, CURL_MAX_BUF_LENGTH < r ? CURL_MAX_BUF_LENGTH : r );
-		std::cout<<in;
+		std::cout << in;
 		return r;
 	}
 
@@ -46,10 +46,52 @@ public:
 };
 
 class SpiderStocksOverview:Spider{
+private:
+	const char *stock_filter[2] = {"0", "6"};
+
 public:
 	SpiderStocksOverview() = default;
-	void test(){
-		getWebContent("http://51.push2.eastmoney.com/api/qt/stock/details/sse?fields1=f1,f2,f3,f4&fields2=f51,f52,f53,f54,f55&mpi=2000&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&pos=-0&secid=0.000001&wbp2u=|0|0|0|web");
+
+	int getStockByCode(char *stock_code){
+		return 0;	
+	}
+
+	int getAllStocks(){
+		#define _FIELDS "fields="
+		#define _BASE_URL "http://45.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112405393508833921838_1666529170574"
+		#define _PARAMS "&pn=1&pz=1&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048&"
+
+		#define _STOCK_NAME_KEY "f15"
+		#define _STOCK_CODE_KEY "f12"
+		#define _STOCK_NEWST_PRICE_KEY "f2"
+		#define _STOCK_TURNOVER_RATE_KEY "f3"
+		#define _STOCK_TURNOVER_KEY "f6"
+		#define _STOCK_AMPLITUDE_KEY "f7"
+		#define _STOCK_HIGEST_PRICE_KEY "f15"
+		#define _STOCK_LOWST_PRICE_KEY "f16"
+		#define _STOCK_START_PRICE_KEY "f17"
+		#define _STOCK_CHANGEPERCENT_KEY "f8"
+		#define _STOCK_PE_KEY "f9"
+		#define _STOCK_MARKET_VALUE_KEY "f20"
+		#define _STOCK_TRADED_MARKET_VALUE_KEY "f21"
+
+		auto rs = getWebContent( _BASE_URL _PARAMS _FIELDS 
+			 _STOCK_NAME_KEY ","
+			 _STOCK_CODE_KEY ","
+			 _STOCK_NEWST_PRICE_KEY ","
+			 _STOCK_TURNOVER_RATE_KEY ","
+			 _STOCK_TURNOVER_KEY ","
+			 _STOCK_AMPLITUDE_KEY ","
+			 _STOCK_HIGEST_PRICE_KEY ","
+			 _STOCK_LOWST_PRICE_KEY ","
+			 _STOCK_START_PRICE_KEY ","
+			 _STOCK_CHANGEPERCENT_KEY ","
+			 _STOCK_PE_KEY ","
+			 _STOCK_MARKET_VALUE_KEY ","
+			 _STOCK_TRADED_MARKET_VALUE_KEY 
+			"&_=1666529170575"
+		);
+		return 0;	
 	}
 };
 
--- c++/stock_picker/include/stock.hpp
+++ c++/stock_picker/include/stock.hpp
@@ -6,25 +6,32 @@
 #define MAX_LENGTH_STOCK_NAME 48 
 #define MAX_LENGTH_STOCK_ATTR_CONCEPT 28
 
-struct StockAttr{
+struct StockHistoryOverview{
 	int rank;
-	int industries_involved[MAX_LENGTH_STOCK_ATTR_CONCEPT];
-	int departments[MAX_LENGTH_STOCK_ATTR_CONCEPT];
+	int price_start;
+	int price_higest;
+	int price_lowest;
+	int turnover_rate;
+	long turnover_sum;
+	int changepercent;
+	char date[12]; //YYYYMMDD
 };
 
-struct StockTransaction{
-	//time(second) offset
-	int time;
+struct StockHistoryTransaction{
+	//top 10
 	int price;
 	int turnover;
 };
 
 struct Stock{
 	int code;
+	int pe;
+	long market_value;
+	long traded_market_value;
 	char name[MAX_LENGTH_STOCK_NAME];
-	char date[12]; //YYYYMMDD
-	StockAttr attrs;
-	std::vector<StockTransaction> transactions;
+	int departments[MAX_LENGTH_STOCK_ATTR_CONCEPT];
+	StockHistoryOverview history_attr;
+	std::vector<StockHistoryTransaction> transactions;
 };
 
 #endif
