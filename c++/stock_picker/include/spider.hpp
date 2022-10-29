1e49c09d7442faece0dabfa42e1a0eb6bc3a79cc
myster liu <2249470506@qq.com>
2022-10-29 19:49:50 +0800

    get datas

--- c++/stock_picker/include/spider.hpp
+++ c++/stock_picker/include/spider.hpp
@@ -3,9 +3,22 @@
 
 #include<curl/curl.h>
 #include<string.h>
-#include <iostream>
+#include<iostream>
+#include<map>
+#include <sstream>
+#include <time.h>
+
+#include"stock.hpp"
 
 #define CURL_MAX_ERROR_BUF_LENGTH 1000
+
+inline int float_str_to_int(std::string str){
+    return static_cast<int>(std::stof(str) * 100);
+}
+
+inline long float_str_to_long(std::string str){
+    return static_cast<long>(std::stof(str) * 100);
+}
 
 //interface
 class Spider{
@@ -21,14 +34,14 @@ public:
 
 	~Spider(){
 		curl_easy_cleanup(_curl);
-		curl_easy_setopt(_curl, CURLOPT_WRITEDATA, NULL);
 		curl_easy_setopt(_curl, CURLOPT_ERRORBUFFER, _curl_error_buf);
 		curl_easy_setopt(_curl, CURLOPT_NOPROGRESS, 1L);
 		curl_easy_setopt(_curl, CURLOPT_VERBOSE, 0L);
 	}
 	
-	int getWebContent(const char* url,uint (*_writeCb)(char* ,uint ,uint ,char*)){
+	int getWebContent(const char* url,uint (*_writeCb)(char* ,uint ,uint ,char*), void* out_data = NULL){
 		curl_easy_setopt(_curl, CURLOPT_URL, url);
+		curl_easy_setopt(_curl, CURLOPT_WRITEDATA, out_data);
 		curl_easy_setopt(_curl, CURLOPT_WRITEFUNCTION, _writeCb);
 		return curl_easy_perform(_curl);
 	}
@@ -37,8 +50,9 @@ public:
 class SpiderStocksOverview:Spider{
 private:
 	const char *stock_filter[7] = {"000", "600", "601","602","603","604","605"};
+	std::map<int, Stock> stocks;
 
-	static uint _writeCb(char *in, uint size, uint nmemb, char *out){
+	static uint _writeCb(char *in, uint size, uint nmemb, char *out_interface){
 		enum struct JsonState{
 			BeforeStart,
 			LookForKey,
@@ -47,10 +61,14 @@ private:
 			InValue,
 		};
 
+		/* TODO 完全可以优化成类成员，从而支持多线程，不过暂时没必要 */
 		static JsonState state = JsonState::BeforeStart;
 		static char key[100],value[100];
+		static std::map<std::string, std::string> entries;
 		static int key_index,value_index;
 		static int find_item = 0;
+		
+		auto* spiderStocksOverview = reinterpret_cast<SpiderStocksOverview*>(out_interface);
 
 		while(*in){
 			switch(state){
@@ -80,18 +98,26 @@ private:
 					}
 					break;
 				case JsonState::InValue:
-					if(*in ==',' || *in == '}'){ 
+					if(*in == ',' || *in == '}'){ 
 						find_item = 1;
 						state = JsonState::LookForKey;
-					}else{
+					}else if(*in != '\"'){
 						value[value_index++] = *in;
 						value[value_index] = '\0';
 					}
 					break;
 			}
 			if(find_item){
+				entries[key] = value;
+				if(*in == '}'){
+					for(const auto* stock_code:spiderStocksOverview->stock_filter ){
+						if(strcmp(stock_code, value)){
+							spiderStocksOverview->addStock(entries);
+							break;
+						}
+					}
+				}
 				find_item = 0;
-				std::cout << key << ":" << value << std::endl;
 			}
 			in++;
 		}
@@ -103,11 +129,8 @@ private:
 public:
 	SpiderStocksOverview() = default;
 
-	int getStockByCode(char *stock_code){
-		return 0;	
-	}
-
 	int getAllStocks(){
+		/* TODO 这显然不是一个长期的接口 */
 		#define _FIELDS "fields="
 		#define _BASE_URL "http://45.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112405393508833921838_1666529170574"
 		#define _PARAMS "&pn=1&pz=1&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048&"
@@ -141,9 +164,37 @@ public:
 			 _STOCK_MARKET_VALUE_KEY ","
 			 _STOCK_TRADED_MARKET_VALUE_KEY 
 			"&_=1666529170575"
-		,_writeCb);
+		,_writeCb, this);
 		return 0;	
 	}
+
+	int addStock(std::map<std::string, std::string> &stock_attr){
+		int stock_code = std::stoi(stock_attr[_STOCK_CODE_KEY]);
+		
+		time_t t = time(0);
+		char tmp[12];
+		
+		strftime(tmp, sizeof(tmp), "%Y-%m-%d", localtime(&t));
+		std::string date(tmp);
+
+		this->stocks[stock_code] = Stock().setName(stock_attr[_STOCK_NAME_KEY])
+										.setPe(float_str_to_int(stock_attr[_STOCK_PE_KEY]))
+										.setMarketValue(std::stol(stock_attr[_STOCK_MARKET_VALUE_KEY]))
+										.setTradedMarketValue(std::stol(stock_attr[_STOCK_TRADED_MARKET_VALUE_KEY]))
+										;
+		this->stocks[stock_code].getHistoryAttr().setPriceStart(float_str_to_int(stock_attr[_STOCK_START_PRICE_KEY]))
+												.setPriceNewst(float_str_to_int(stock_attr[_STOCK_NEWST_PRICE_KEY]))
+												.setPriceHigest(float_str_to_int(stock_attr[_STOCK_HIGEST_PRICE_KEY]))
+												.setPriceLowest(float_str_to_int(stock_attr[_STOCK_LOWST_PRICE_KEY]))
+												.setAmplitude(float_str_to_int(stock_attr[_STOCK_AMPLITUDE_KEY]))
+												.setTurnoverRate(float_str_to_int(stock_attr[_STOCK_TURNOVER_RATE_KEY]))
+												.setTurnoverSum(float_str_to_int(stock_attr[_STOCK_TURNOVER_KEY]))
+												.setChangePercent(float_str_to_int(stock_attr[_STOCK_CHANGEPERCENT_KEY]))
+												.setDate(date.c_str())
+										;
+		return 0;	
+	}
+
 };
 
 #endif
--- c++/stock_picker/include/stock.hpp
+++ c++/stock_picker/include/stock.hpp
@@ -2,36 +2,62 @@
 #define _STOCK_CLASS
 
 #include<vector>
+#include<string.h>
 
-#define MAX_LENGTH_STOCK_NAME 48 
 #define MAX_LENGTH_STOCK_ATTR_CONCEPT 28
 
 struct StockHistoryOverview{
 	int rank;
 	int price_start;
+	int price_newst;
 	int price_higest;
 	int price_lowest;
+	int amplitude;
 	int turnover_rate;
 	long turnover_sum;
-	int changepercent;
+	int change_percent;
 	char date[12]; //YYYYMMDD
+
+	StockHistoryOverview& setRank(int rank){this->rank = rank;return *this;}
+	StockHistoryOverview& setPriceStart(int price_start){this->price_start = price_start;return *this;}
+	StockHistoryOverview& setPriceNewst(int price_newst){this->price_newst = price_newst;return *this;}
+	StockHistoryOverview& setPriceHigest(int price_higest){this->price_higest = price_higest;return *this;}
+	StockHistoryOverview& setPriceLowest(int price_lowest){this->price_lowest = price_lowest;return *this;}
+	StockHistoryOverview& setAmplitude(int amplitude){this->amplitude = amplitude;return *this;}
+	StockHistoryOverview& setTurnoverRate(int turnover_rate){this->turnover_rate = turnover_rate;return *this;}
+	StockHistoryOverview& setTurnoverSum(int turnover_sum){this->turnover_sum = turnover_sum;return *this;}
+	StockHistoryOverview& setChangePercent(int change_percent){this->change_percent = change_percent;return *this;}
+	StockHistoryOverview& setDate(const char date[12]){strncpy(this->date, date, 12);return *this;}
 };
 
 struct StockHistoryTransaction{
 	//top 10
 	int price;
 	int turnover;
+
+	StockHistoryTransaction(int price, int turnover):price(price), turnover(turnover){}
 };
 
 struct Stock{
-	int code;
+private:
+	int departments_index;
+public:
 	int pe;
 	long market_value;
 	long traded_market_value;
-	char name[MAX_LENGTH_STOCK_NAME];
+	std::string name;
 	int departments[MAX_LENGTH_STOCK_ATTR_CONCEPT];
 	StockHistoryOverview history_attr;
 	std::vector<StockHistoryTransaction> transactions;
+
+	Stock& setPe(int pe){this->pe = pe;return *this;}
+	Stock& setMarketValue(long market_value){this->market_value = market_value;return *this;}
+	Stock& setTradedMarketValue(long traded_market_value){this->traded_market_value = traded_market_value;return *this;}
+	Stock& setName(const std::string name){this->name = name;return *this;}
+	Stock& addDp(int dp){this->departments[this->departments_index++] = dp;return *this;}
+	Stock& addTransaction(int price, int turnover){this->transactions.push_back(StockHistoryTransaction(price, turnover));return *this;}
+
+	StockHistoryOverview& getHistoryAttr(){return this->history_attr;}
 };
 
 #endif
